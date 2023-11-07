cd3f13ee1a018e20e3fcaa9549bccc56cf1c00fb
macpro <macpro@192.168.1.3>
2023-11-07 18:09:29 +0700

    add class ok

---
+++ src/asm_ph32091/kokok.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package asm_ph32091;
+
+/**
+ *
+ * @author macpro
+ */
+public class kokok {
+    
+}
