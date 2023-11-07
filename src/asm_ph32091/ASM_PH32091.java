ed289b767cf8a99bccb66ac839e30c96817e2589
macpro <macpro@192.168.1.3>
2023-11-07 18:03:15 +0700

    add file

---
+++ src/asm_ph32091/ASM_PH32091.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package asm_ph32091;
+
+/**
+ *
+ * @author macpro
+ */
+public class ASM_PH32091 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
